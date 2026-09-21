a3f3d28c0ceed0601e6b42c7ae43e13f28a5a961
ivantan2019 <ivantan@smu.edu.sg>
2026-09-21 16:12:27 +0800

    added duck files

---
+++ test_duckfine.py
@@ -0,0 +1,33 @@
+import unittest
+
+from duckfine import DuckFine
+
+
+class TestDuckFine(unittest.TestCase):
+
+    def setUp(self):
+        self.fine = DuckFine("M001")
+
+    def test_charge_returns_a_fee_for_a_late_duck(self):
+        self.assertEqual(self.fine.charge(5), 1.50)
+
+    def test_deluxe_duck_costs_more(self):
+        self.assertTrue(self.fine.charge(5, deluxe=True) > 0)
+
+    def test_no_fee_when_returned_on_time(self):
+        self.assertEqual(self.fine.charge(0), 0.0)
+
+    def test_negative_days_raises(self):
+        with self.assertRaises(ValueError):
+            self.fine.charge(-1)
+
+    def test_fine_is_capped_at_five_dollars(self):
+        self.assertEqual(self.fine.charge(20), 5.00)
+
+    def test_fines_accumulate_against_member_account(self):
+        self.fine.charge(5)   # $1.50
+        self.fine.charge(4)   # $1.00
+        self.assertEqual(self.fine.total_owed, 2.50)
+
+if __name__ == "__main__":
+    unittest.main()
